b20978e6bc57417cb2df3a19217e70c37c5f6acc
Mausham Bista <maushambista0@gmail.com>
2023-11-15 19:59:06 -0700

    created the final draft

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+all_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+special_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', ';', ':', ',', '.', '<', '>', '/', '?', '|']
+
+def generate_password():
+    rn_letters = random.randint(6,8)
+    rn_numbers = random.randint(2,4)
+    rn_symbols = random.randint(2,4)
+
+    pasword_letters=[random.choice(all_letters) for char in range(rn_letters)]
+    pasword_numbers=([random.choice(numbers) for char in range(rn_numbers)])
+    pasword_symbols=[random.choice(special_characters) for char in range(rn_symbols)]
+
+    password_list = pasword_letters + pasword_numbers + pasword_symbols
+    random.shuffle(password_list)
+
+    new_password = "".join(password_list)
+    password_entry.insert(0, new_password)
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-
-
-
-    
 def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
 
-    with open('password.txt', 'a') as filename:
-        filename.write(f"{website} | {email} | {password}\n")
+   
+    if len(website) == 0 or len(password) == 0:
+        is_full = messagebox.showinfo(title="Opps", message="Please do not leave any fields empty.")
 
-    website_entry.delete(0,  END)
-    email_entry.delete(0, END)
-    password_entry.delete(0, END)
+    else:
+        is_ok= messagebox.askokcancel(title="Sure?", message=f"These are the details entered: \nEmail: {email}\nPassword: {password}\nIs it okay to save?")
+        if is_ok:
+            with open('password.txt', 'a') as filename:
+                filename.write(f"{website} | {email} | {password}\n")
+
+            website_entry.delete(0,  END)
+            email_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -55,7 +80,8 @@ password_entry = Entry(width= 21)
 password_entry.grid(column=1, row=3)
 
 
-generate_password = Button(text="Genrate Password")
+
+generate_password = Button(text="Genrate Password", command=generate_password)
 generate_password.grid(column = 2, row=3)
 
 add = Button(text="ADD", width=36, command= save_password)
